f9322ae5800b58db706ccfe923d05a1dbc956b3f
ansolotli <annaliinakoskinen@gmail.com>
2020-01-30 12:25:49 +0200

    Updated texts

--- shiny/ui.R
+++ shiny/ui.R
@@ -33,7 +33,7 @@ sidebar <- dashboardSidebar(
                            choices = c("Sustainable" = "Sus", "Pelagics-Focused" = "Pel", "Open Access" = "OA"), 
                            selected = "Sus"
                ), "Fishery policies", 
-               content = "Sustainable fishery policy explores if healthy states of all three major fish stocks (i.e. cod, herring and sprat) will support a healthy and functioning food web and will be the basis for developing a sustainable exploitation of the Baltic Sea. <br><br> Pelagics-Focused scenario explores the option that given the climate change and nutrient load scenarios, prioritisation of herring and sprat over cod would be the preferred fisheries management strategy. <br><br> Open access scenario enables exploring the impacts of largely unregulated fisheries management on the status of the fish stocks, the food web and the economic outcomes from fisheries in the future Baltic Sea."
+               content = "Sustainable fishery policy explores if this option would provide healthy states of all three major fish stocks (i.e. cod, herring and sprat) as well as support a healthy and functioning food web, therefore providing a basis for developing a sustainable exploitation of the Baltic Sea. <br><br> Pelagics-Focused scenario explores the option that given the climate change and nutrient load scenarios, prioritisation of herring and sprat over cod would be the preferred fisheries management strategy. <br><br> Open access scenario enables exploring the impacts of largely unregulated fisheries management on the status of the fish stocks, the food web and the economic outcomes from fisheries in the future Baltic Sea."
                , placement = "right", trigger = "hover", 
                #increase the size of the popover according to the length of content
                options = list(container = "body")),
@@ -43,7 +43,7 @@ sidebar <- dashboardSidebar(
                            choices = c("Baltic Sea Action Plan" = "BSAP", "Average 1995-2002" = "Ref"),
                            selected = "BSAP"
                ), "Nutrient loading policies", 
-               content = "Nutrient loads were modelled according to the higher reference conditions between 1995 and 2002 and according to the lower nutrient loads outlined in the Baltic Sea Action Plan programme by HELCOM."
+               content = "Nutrient loads were modelled both according to the higher reference conditions between 1995 and 2002 and the lower nutrient loads outlined in the Baltic Sea Action Plan programme by HELCOM (Baltic Marine Environment Protection Commission – Helsinki Commission)."
                , placement = "right", trigger = "hover", options = list(container = "body")),
                
                popify(selectInput(inputId = 'Climate', 
@@ -51,7 +51,7 @@ sidebar <- dashboardSidebar(
                            choices = c("RCP4.5", "RCP8.5"), 
                            selected = "RCP4.5"
                ), "Climate scenarios", 
-               content = "Climate scenarios follow Representative Concentration Pathways (RCP) 4.5 and 8.5 according to the ICPP 2014."
+               content = "Climate scenarios follow Representative Concentration Pathways (RCP) 4.5 and 8.5 according to the ICPP 2014. These describe the greenhouse gas concentrations in the atmosphere, with higher concentrations resulting in higher warming effect on earth."
                , placement = "right", trigger = "hover", options = list(container = "body")),
                
                # Create the two colums of checkbox groups (biomass and catch)
@@ -99,13 +99,13 @@ sidebar <- dashboardSidebar(
                                            choiceValues = list("plotRv", "plotTemp1", "plotTemp2", "plotHyp"),
                                            selected = "plotRv"), 
                                title = "Novelty variables", 
-                               content = "Cod reproductive volume is the water layer where the water is warm enough but not too saline for the eggs to float in an oxygenated environment. If this volume decreases, the reproduction of cod will be less successful. <br><br> The temperature variables present the water temperature in the surface layer of the water column during spring months and in the summer. <br><br> Inverse hypoxic area is an area that has gone anoxic i.e. is not oxygenated."
+                               content = "Cods spawn in the open sea and the eggs drift in the water. The reproductive volume indicates changes in the size of the water layer where salinity is high  enough and thereby also the density of the water (weight of water per its unit volume) is high enough to prevent the eggs from sinking in the oxygen deficient deep water .Eutrophication and less saline water inflow to the Baltic Sea via the Danish Straits tend to decrease the reproductive volume of cod. <br><br> The temperature variables present the water temperature in the surface layer of the water column during spring months and in the summer. <br><br> Inverse hypoxic area is the inverse of the deep water area that has gone hypoxic i.e. this variable describes the proportion of the study area where oxygen concentration in the deep water is above 2 mg/l."
                                , placement = "right", trigger = "hover", options = list(container = "body")),
                         
                         popify(checkboxInput(inputId = "novelTotal", 
                                       label = "Total abiotic novelty", value = TRUE),
                                title = "Total abiotic novelty", 
-                               content = "Total abiotic novelty is the sum of other novelty variables and describes the total novelty expected to occur in the Central Baltic Sea in the current century."
+                               content = "Total abiotic novelty is the sum of other novelty variables (five water quality variables, each scaled between 0 and 1, 0 denoting values falling in the 95% confidence interval from observational data, and 1 denoting the extreme values of the model predictions till the end of the 21st century) and describes the total novelty expected to occur in the Central Baltic Sea in the current century."
                                , placement = "right", trigger = "hover", options = list(container = "body"))
                  )
                )
@@ -118,7 +118,7 @@ sidebar <- dashboardSidebar(
                                        choices = c("No profit", "Profit larger than 0", "Profit larger than 100", "Profit larger than 200"),
                                        selected = "Profit larger than 100"
                ), title = "Minimum acceptable profit", 
-               content = "Minimum acceptable profit enables selection of the desired lowest limit of profitability of fisheries on cod, herring, and sprat in millions of euros."
+               content = "Minimum acceptable profit enables selection of the desired lowest limit of annual profitability of fisheries on cod, herring, and sprat in millions of euros."
                , placement = "right", trigger = "hover", options = list(container = "body")),
                
                popify(selectInput(inputId = 'F_GES',
@@ -135,7 +135,8 @@ sidebar <- dashboardSidebar(
                                    choices = c("Blim" = "Blim", "B MSY trigger" = "B MSY"),
                                    selected = "Blim",
                                    inline = TRUE # horizontal display
-               ), title = "Reference point", content = "Blim is the reference point identified by ICES describing the limit below which there is a high risk of reduced recruitment. <br><br> B MSY trigger (or BMSYtrigger) is considered as the lower bound of spawning–stock biomass fluctuation below which fishing needs to be reduced to allow a fish stock to rebuild to levels capable of producing maximum sustainable yield (MSY)."
+               ), title = "Reference point", 
+               content = "Blim is the reference point describing the limit below which there is a high risk of reduced recruitment. <br><br> B MSY trigger (or BMSYtrigger) is considered as the lower bound of spawning–stock biomass fluctuation below which fishing needs to be reduced to allow a fish stock to rebuild to levels capable of producing maximum sustainable yield (MSY)."
                , placement = "right", trigger = "hover", options = list(container = "body")),
                
                popify(selectInput(inputId = 'Nutr_GES',
@@ -143,7 +144,7 @@ sidebar <- dashboardSidebar(
                                        choices = c("Above GES" = "GES", "Below GES" = "Sub-GES"),
                                        selected = "Above"
                ), title = "Status of water quality", 
-               content = "The indicators of water quality comprise of the average concentrations of nitrogen, phosphorus and chlorophyll <i>a</i>. Low concentrations suggest high probability of reaching reaching the Good Environmental Status (GES)."
+               content = "The indicators of water quality comprise of the average concentrations of nitrogen, phosphorus and chlorophyll <i>a</i>. Low concentrations suggest high probability of reaching the Good Environmental Status (GES) as defined by HELCOM (Baltic Marine Environment Protection Commission – Helsinki Commission)."
                , placement = "right", trigger = "hover", options = list(container = "body")),
                 
                popify(radioButtons(inputId = 'Novelty',
@@ -217,7 +218,7 @@ body <- dashboardBody(
     )
     ,
     tabItem("optimize",
-            titlePanel("Explore the forecasts about the ecosystem services"),
+            titlePanel("Explore the benefits of different scenario outcomes"),
             fluidRow(
               column(width = 12,
                      uiOutput("opt_info"),
